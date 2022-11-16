b68a1b8c3a02b5eeda9606923a0569f0898044f4
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-16 13:02:16 -0600

    set the key and value to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
+  let buttons = $(".saveBtn");
+
+  buttons.on("click", function (event) {
+    let parent = $(this).closest(".time-block").attr("id");
+    let description = $(this).siblings(".description").val();
+    console.log(parent, description);
+    localStorage.setItem(parent, description);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +14,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
