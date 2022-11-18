08316297c7d0d4a55e61b2d077e47f3dc723456b
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-18 00:05:16 -0600

    added a clock to my application

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,19 @@
-$(function () {
-  let buttons = $(".saveBtn");
+let time = dayjs().format("h:mm:ss");
+$("#timer").text(time);
 
+$(function () {
+  let descriptionEl = $(".description");
+  let buttons = $(".saveBtn");
   buttons.on("click", function (event) {
     let parent = $(this).closest(".time-block").attr("id");
     let description = $(this).siblings(".description").val();
     console.log(parent, description);
     localStorage.setItem(parent, description);
   });
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
+  function init() {
+    JSON.parse(localStorage.setItem(parent));
+  }
+  init();
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
