50b182cc9597df13e84ad217f729ee0c0d96b84c
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-19 12:29:50 -0600

    fixed bug with a div

--- Develop/script.js
+++ Develop/script.js
@@ -4,22 +4,22 @@ let hour = dayjs().hour();
 let currentHour = dayjs().format("HH");
 
 $("#day").text(day);
-$("#timer").text(currentHour);
+$("#timer").text(time);
 
 let hoursArray = JSON.parse(localStorage.getItem("Hours")) || [];
 
 function init() {
-  let index = 0;
-  hoursArray.forEach((hourObject) => {
-    if ("hour-" + index === hourObject.parent) {
-      $(".description").text(hourObject.description);
-      console.log(hourObject);
-    }
-    index++;
-  });
+  // hoursArray.forEach((hourObject) => {
+  // let timeDiv = $("time-block").attr("id").split("-")[1];
+
+  //   if (timeDiv == hourObject.parent.split("-")[1]) {
+  //     $(".description").text(hourObject.description);
+  //   }
+  // });
 
   $(".time-block").each(function () {
     let timeDiv = $(this).attr("id").split("-")[1];
+    console.log(timeDiv);
 
     if (currentHour === timeDiv) {
       $(this).addClass("present");
@@ -41,7 +41,7 @@ $(function () {
       parent: $(this).closest(".time-block").attr("id"),
       description: $(this).siblings(".description").val(),
     };
-    console.log(hourObject.parent, hourObject.description);
+    console.log(hourObject.parent.split("-")[1], hourObject.description);
 
     hoursArray.push(hourObject);
 
@@ -50,12 +50,7 @@ $(function () {
   });
 
   init();
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
