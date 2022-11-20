5984b10eb0a5d88feee260fb57907cc002648d56
Amer Kulovic <amer.kulovic@yahoo.com>
2022-11-19 18:13:23 -0600

    Polished up the formatting

--- Develop/script.js
+++ Develop/script.js
@@ -2,29 +2,16 @@ let time = dayjs().format("h:mm:ss");
 let day = dayjs();
 let hour = dayjs().hour();
 let currentHour = dayjs().format("HH");
-
-$("#day").text(day);
-$("#timer").text(time);
-
 let hoursArray = JSON.parse(localStorage.getItem("Hours")) || [];
+
+$("#day").text(day.format("MMM D, YYYY"));
+$("#timer").text(time);
 
 function init() {
   for (let i = 0; i < hoursArray.length; i++) {
     let time = hoursArray[i].parent.split("-")[1];
     $(`#${time}`).val(hoursArray[i].description);
   }
-  // let timeBlock = $(".time-block");
-
-  // for (let i = 0; i < timeBlock.length; i++) {
-  //   let timeDiv = timeBlock[i].getAttribute("id").split("-")[1];
-  //   if (currentHour === timeDiv) {
-  //     timeBlock[i].classList.add("present");
-  //   } else if (currentHour > timeDiv) {
-  //     timeBlock[i].classList.add("past");
-  //   } else {
-  //     timeBlock[i].classList.add("future");
-  //   }
-  // }
 
   $(".time-block").each(function () {
     let timeDiv = $(this).attr("id").split("-")[1];
